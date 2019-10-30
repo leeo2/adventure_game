443d30bc3675689e83f539ef433de77863dbd0a1
fullere <fullere@hartwick.edu>
2019-10-30 16:55:05 -0400

    continued swamp path. read comments about trap/monster spawn chance.

--- adventure_game.py
+++ adventure_game.py
@@ -13,28 +13,66 @@ def die_roll():
 
 
 def chance_roll():
-    # either dice roll, or random int for chance of trap, monster, or free path
+    # either dice roll, or random int for chance of trap, monster, or free path. change if statements if values change
     chance = random.randint(1, 10) # what range do we want to use? if dice 2-12? if not is 1-10 fine?
     return chance
 
 
 def swamp_path():
+    # Path to go to the swamp. walking down chosen path, first swamp path fork dialog
     print("You walk down the path taking in the scenery.\nYou come across another fork in the road.")
     print("The path splits left and right.\nThe right path is overgrown and looks very untrodden.")
-    print("The left path is poorly lit from the tree canopy. Better light a torch! ")
+    print("The left path is poorly lit from the tree canopy. Better light a torch!")
     print("Which way do you go:\nRight [1]\nLeft [2]")
+    # ask for player choice 3 options, right, left, and get lost by leaving the path
+    #
     swamp_choice1 = input("> ")
     swamp_choice1 = swamp_choice1.strip()
-    swamp_choice1 = input(swamp_choice1)
-    if swamp_choice1 == 1:
-        print("Went right")  # place holder text
-    elif swamp_choice1 == 2:
-        print("Went Left")
-    else:
-        print("Um... this is not the path... turn back... great now your lost")
 
+    if swamp_choice1 == "1":
+        # higher monster chance
+        print("You took the path on the right.\nYou carefully watch your step as to not get tangled in the undergrowth.")
+        # determine if monster appears, trap activates, or if player misses any impeding obstacles
+        spawn_right1 = chance_roll()
+        print(spawn_right1) # statement to check random int
+        if spawn_right1 <= 3:
+            # free
+            print("no monsters or traps")
+            print("""The undergrowth almost trips you up and you find a rare flower. 
+Nothing else of note happens as you travel down the path. 
+Eventually you come across a clearing.""")
+        elif spawn_right1 <= 6:
+            # trap
+            print("Trap activates")
+        else:
+            # monster
+            print("Monster appears")
+
+    elif swamp_choice1 == "2":
+        # higher trap chance
+        print("Went Left")
+        spawn_left1 = chance_roll()
+        print(spawn_left1)
+    else:
+        # left the path and get lost. options from here- end game or possibly find next path split
+        print("Um... this is not the path... turn back... great now your lost")
+    # paths converge and then there is another fork with 2 paths
+    print("You decide to take a quick break and look around the clearing.")
+    print("""As you do you realize the other path probably ends here as well. 
+You could have taken either and still gotten here!""")
+    print("It looks like there is a swamp up ahead. Gross... mud and icky things everywhere.")
     print("One path goes of to the right and looks very marshy. It doesn't look very stable, watch your step!")
     print("The other veers off to the left. This one looks more stable and considerably drier.")
+    print("Which path do you think you should take?\nRight [1]\nLeft [2]")
+    swamp_choice2 = input("> ")
+    swamp_choice2 = swamp_choice2.strip()
+    if swamp_choice2 == "1":
+        print("Right")
+    elif swamp_choice2 == "2":
+        print("Left")
+    else:
+        print("Other action")
 
 
-swamp_path() # function caller to test swamp path
+
+run = swamp_path() # function caller to test swamp path
